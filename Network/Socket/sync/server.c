5fade1a3fd4b7f793e1b0b6724ec74693fd08533
Maochuan <thinkermao@outlook.com>
2017-03-25 10:49:22 +0800

    _

--- Network/Socket/sync/server.c
+++ Network/Socket/sync/server.c
@@ -9,6 +9,44 @@ typedef struct sockaddr *SA;
 
 #define SERVER_PORT 1234
 
+ssize_t readn(int fd, void *vptr, size_t n)
+{
+    char *ptr = vptr;
+    size_t nleft = n;
+    whlie (nleft > 0) {
+        ssize_t nread = read(fd, ptr, nleft);
+        if (nread < 0) {
+            if (errno == EINTR) 
+                nread = 0;  /* and call read() again */
+            else 
+                return -1;  
+        }
+        else if (nread == 0) 
+            break;          /* EOF */
+        nleft -= nread;
+        ptr += nread;
+    }
+    return n - nread;   /* return >= 0 */
+}
+
+ssize_t writen(int fd, const void *vptr, size_t n) 
+{
+    char *ptr = vptr;
+    size_t nleft = n;
+    while (nleft > 0) {
+        ssize_t nwritten = write(fd, ptr, n);
+        if (nwritten) <= 0) {
+            if (nwritten < 0 && errno == EINTR)
+                nwritten = 0;   /* and call wirte() again */
+            else
+                return (-1);
+        }
+        nleft -= nwritten;
+        ptr += nwritten;
+    }
+    return (n);
+}
+
 void doit(int sockfd) 
 {
     printf("accept socket fd: %d.\n", sockfd);
