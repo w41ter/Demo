386f079045ef5ddac83a238bd7862c995dd31554
Maochuan <thinkermao@outlook.com>
2017-03-25 10:15:45 +0800

    remove exe

--- ACM/contest/1/test.bat
+++
@@ -1,7 +0,0 @@
-:loop
-test.exe
-i.exe
-i2.exe
-fc a.txt b.txt
-if %errorlevel%==0 goto loop
-pause 
--- ACM/contest/2/test.bat
+++
@@ -1,7 +0,0 @@
-:loop
-test.exe
-b.exe
-b1.exe
-fc a.txt b.txt
-if %errorlevel%==0 goto loop
-pause 
